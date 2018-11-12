a355ce4491c19b65c3e956988024178cbd9aec47
Healthedata1 <ehaas@healthedatainc.com>
2018-11-12 12:51:51 -0800

    publish content

---
+++ New_US_Core_Diagnostic_Report_Profile_for_Report_and_Note_exchange.java
@@ -0,0 +1,7 @@
+package http://hl7.org/fhir/us/core-r4/ImplementationGuide/ig;
+
+import org.hl7.fhir.r4.model.ProfilingWrapper;
+
+public class New_US_Core_Diagnostic_Report_Profile_for_Report_and_Note_exchange {
+
+}
